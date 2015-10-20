5ca1630c6c44082c460616e908a91d904c193caf
Tony Kirke <tony@pyspuc.com>
2015-10-19 22:36:17 -0700

    Use enum for filter types instead of a boolean for low/high pass

--- spuce/filters/chebyshev_iir.cpp
+++ spuce/filters/chebyshev_iir.cpp
@@ -12,7 +12,7 @@ void chebyshev_iir(iir_coeff& filt, float_type fcd, float_type ripple = 3.0) {
 	float_type rlin = pow(ten, ripple/ten);
 	float_type epi  = sqrt(rlin - 1.0);
   //! wca - pre-warped angular frequency
-  float_type wca = (filt.get_type()) ? tan(M_PI * fcd) : tan(M_PI*(0.5-fcd));
+  float_type wca = (filt.get_type()==filter_type::low) ? tan(M_PI * fcd) : tan(M_PI*(0.5-fcd));
   auto n2 = (order + 1) / 2;
   chebyshev_s(filt, wca, epi, order, n2);
   filt.bilinear();
@@ -20,24 +20,22 @@ void chebyshev_iir(iir_coeff& filt, float_type fcd, float_type ripple = 3.0) {
 }
 //! Calculate poles (chebyshev)
 void chebyshev_s(iir_coeff& filt, float_type wp, float_type epi, long n, long n2) {
-  auto l = 0;
-  if (n % 2 == 0) l = 1;
+  auto l = 1;
   float_type arg;
   float_type x = 1 / epi;
-  float_type asinh = log(x + sqrt(1.0 + x * x));
-  float_type v0 = asinh / (float_type(n));
-  float_type sm = sinh(v0);
-  float_type cm = cosh(v0);
+  float_type lambda = pow(x*(1.0 + sqrt(1.0 + epi*epi)),1.0/n);
+  float_type sm = 0.5*((1.0/lambda) - lambda);
+  float_type cm = 0.5*((1.0/lambda) + lambda);
   for (int j = 0; j < n2; j++) {
-    arg = -0.5 * M_PI * l / ((float_type)(n));
-    if (filt.get_type()) {
-      filt.set_pole(wp * std::complex<float_type>(-sm * sin(arg), cm * cos(arg)), j);
-      filt.set_zero(FLT_MAX, j);
+    arg = 0.5 * M_PI * (2*l-1) / ((float_type)(n));
+    if (filt.get_type()==filter_type::low) {
+			filt.set_pole(wp * std::complex<float_type>(sm * sin(arg), cm * cos(arg)), n2-1-j);
+      filt.set_zero(FLT_MAX, n2-1-j);
     } else {
-      filt.set_pole(1.0 / (wp * std::complex<float_type>(-sm * sin(arg), cm * cos(arg))), j);
-      filt.set_zero(0, j);
+      filt.set_pole(1.0 / (wp * std::complex<float_type>(sm * sin(arg), cm * cos(arg))), n2-1-j);
+      filt.set_zero(0, n2-1-j);
     }
-    l += 2;
+    l++;
   }
 }
 }  // namespace spuce
